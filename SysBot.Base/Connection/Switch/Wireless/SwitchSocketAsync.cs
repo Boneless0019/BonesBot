a2b63ebc1058532a376ce88145e62d513f688501
Koi <61223145+Koi-3088@users.noreply.github.com>
2026-03-01 21:49:09 -0600

    Implement USB-side of safe reads, catch and log exceptions from bad reads to maintain connection. (#224)

--- SysBot.Base/Connection/Switch/Wireless/SwitchSocketAsync.cs
+++ SysBot.Base/Connection/Switch/Wireless/SwitchSocketAsync.cs
@@ -82,7 +82,7 @@ public sealed class SwitchSocketAsync : SwitchSocket, ISwitchConnectionAsync
             try
             {
                 var mem = buffer.AsMemory()[..size];
-                await Connection.ReceiveAsync(mem, token);
+                await Connection.ReceiveAsync(mem, token).ConfigureAwait(false);
                 return DecodeResult(mem, length);
             }
             finally
@@ -472,7 +472,7 @@ public sealed class SwitchSocketAsync : SwitchSocket, ISwitchConnectionAsync
         {
             await SendAsync(command, token).ConfigureAwait(false);
             var buffer = new byte[length];
-            await Connection.ReceiveAsync(buffer, token);
+            await Connection.ReceiveAsync(buffer, token).ConfigureAwait(false);
             return buffer;
         }
         catch (Exception ex)
